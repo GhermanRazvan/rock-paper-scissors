3e78742197fae9a887c8c7306720be717b86fe90
GhermanRazvan <ghermanrazvan2121@gmail.com>
2023-11-25 18:42:02 +0200

    Created a new way of the computer getting a choice

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-function getComputerChoice(){
-    let items = ["Rock", "Paper", "Scissors"];
+items = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(a){
     let pick =  Math.floor(Math.random() * items.length);
-    console.log("Alegerea este ", items[pick])
+    return a[pick];
+
+}
+
+let randomChoice = getComputerChoice(items)
+console.log(randomChoice)
+
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = "Rock"
+    computerSelection = getComputerChoice();
+    if (playerSelection != computerSelection){
+        console.log("Ai pierdut");
+    }
 }
 
