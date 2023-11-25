259ae9815889064baea51312e008b73ca8fcfc9a
GhermanRazvan <ghermanrazvan2121@gmail.com>
2023-11-25 19:03:09 +0200

    Created a function playRound with each possibility

--- main.js
+++ main.js
@@ -7,14 +7,36 @@ function getComputerChoice(a){
 }
 
 let randomChoice = getComputerChoice(items)
-console.log(randomChoice)
-
+// console.log("Computerul a ales",randomChoice)
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = "Rock"
-    computerSelection = getComputerChoice();
-    if (playerSelection != computerSelection){
-        console.log("Ai pierdut");
+    if (playerSelection == "Rock" && computerSelection == "Rock"){
+        return "Tie"
+    }else if (playerSelection == "Rock" && computerSelection == "Paper"){
+        return "You lost! Paper beats rock"
+    }else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+        return "You won! Rock beats scissors"
+    }else if (playerSelection == "Paper" && computerSelection == "Paper"){
+        return "Tie"
+    }else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        return "You lost! Scissors beats paper"
+    }else if (playerSelection == "Paper" && computerSelection == "Rock"){
+        return "You won! Paper beats rock"
+    }else if (playerSelection == "Scissors" && computerSelection == "Scissors"){
+        return "Tie"
+    }else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        return "You won! Scissors beats paper"
+    }else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+        return "You lost! Rock beats scissors"
     }
 }
+
+
+let playerSelection = prompt("Alege un item");
+console.log("Alegerea jucatorului este", playerSelection);
+let computerSelection = randomChoice;
+console.log("Computerul a ales",randomChoice);
+console.log(playRound(playerSelection, computerSelection));
+
+
 
