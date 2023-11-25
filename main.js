0dd42b002d19efcee693c288c27cf518826cbf4d
GhermanRazvan <ghermanrazvan2121@gmail.com>
2023-11-25 20:26:06 +0200

    Remade the code to be more readable and corrected some mistakes

--- main.js
+++ main.js
@@ -9,47 +9,39 @@ function getComputerChoice(a){
 }
 
 
-function playRound(playerSelection, computerSelection){
+function playRound(){
+    
     let randomChoice = getComputerChoice(items)
-    playerSelection = prompt("Alege un item");
-    console.log("Alegerea jucatorului este", playerSelection);
+    let playerSelection = prompt("Alege un item");
     computerSelection = randomChoice;
+    console.log("Alegerea jucatorului este", playerSelection);
     console.log("Computerul a ales",randomChoice);
-    if (playerSelection == "Rock" && computerSelection == "Rock"){
+
+    if (playerSelection === computerSelection ) {
         return "Tie"
-    }else if (playerSelection == "Rock" && computerSelection == "Paper"){
-        computerScore++;
-        return "You lost! Paper beats rock"
-    }else if (playerSelection == "Rock" && computerSelection == "Scissors"){
-        playerScore++;
-        return "You won! Rock beats scissors"
-    }else if (playerSelection == "Paper" && computerSelection == "Paper"){
-        return "Tie"
-    }else if (playerSelection == "Paper" && computerSelection == "Scissors"){
-        computerScore++;
-        return "You lost! Scissors beats paper"
-    }else if (playerSelection == "Paper" && computerSelection == "Rock"){
-        playerScore++;
-        return "You won! Paper beats rock"
-    }else if (playerSelection == "Scissors" && computerSelection == "Scissors"){
-        return "Tie"
-    }else if (playerSelection == "Scissors" && computerSelection == "Paper"){
-        playerScore++;
-        return "You won! Scissors beats paper"
-    }else if (playerSelection == "Scissors" && computerSelection == "Rock"){
-        computerScore++;
-        return "You lost! Rock beats scissors"
     }
+
+    if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")||
+        (playerSelection === "Paper" && computerSelection === "Rock")){
+            playerScore ++;
+            return "The player won because " + playerSelection + " beats " + computerSelection;
+        } else {
+            computerScore ++;
+            return "The computer won because " + computerSelection + " beats " + playerSelection;
+        }
 }
 
 
 function game(){
-    for ( round = 1; round < 6; round++){
+    for ( round = 1; round <= 5; round++){
+        console.log("Runda", round);
+        console.log(playRound()); 
         console.log("Tu ai scorul", playerScore);
         console.log("Computerul are scorul", computerScore)
-        console.log("Runda", round);
-        console.log(playRound());    
+           
     }
+
     if (playerScore == computerScore ){
         return "The game was a tie";
     }else if (playerScore < computerScore){
@@ -59,6 +51,6 @@ function game(){
     }
 }
 
-game();
-
+finalResult = game();
+console.log(finalResult)
 
